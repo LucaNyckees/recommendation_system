cd550fff09b2ed2d08d9dc8cfac379fef6974f2f
LucaNyckees <nyckees.luca@gmail.com>
2024-11-08 19:18:05 +0100

    fixed nltk download

--- src/nlp/sentiment_analysis/helpers.py
+++ src/nlp/sentiment_analysis/helpers.py
@@ -1,8 +1,20 @@
 from textblob import TextBlob
 import pandas as pd
 from nltk.corpus import stopwords
+# import nltk
+# nltk.download('stopwords')
+
 import nltk
-nltk.download('stopwords')
+import ssl
+
+try:
+    _create_unverified_https_context = ssl._create_unverified_context
+except AttributeError:
+    pass
+else:
+    ssl._create_default_https_context = _create_unverified_https_context
+
+nltk.download()
 stop_words = set(stopwords.words('english'))
 
 
