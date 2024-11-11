c91e7bd60ad10578c8c243b6f2dd8a67d3a38412
LucaNyckees <nyckees.luca@gmail.com>
2024-11-11 16:53:24 +0100

    refactor and added random forest sentiment classifier

--- src/nlp/sentiment_analysis/classifiers.py
+++ src/nlp/sentiment_analysis/classifiers.py
@@ -1,5 +1,6 @@
 from xgboost import XGBClassifier
 from sklearn.metrics import classification_report
+from sklearn.ensemble import RandomForestClassifier
 
 from src.paths import FIGURES_PATH
 from src.processing import DataProcessor
@@ -7,17 +8,25 @@ from src.visualization import make_confusion_matrix
 from src.log.logger import logger
 
 
-class XGBoostSentimentClassifier:
-
-    def __init__(self) -> None:
+class SentimentClassifier:
+    def __init__(self, category: str, embedding: str, frac: float = 0.01) -> None:
         self.trained = False
-        self.model_name = f"xgboost-sentiment-classifier"
+        self.category = category
+        self.embedding = embedding
+        self.frac = frac
 
     def _initialize_data(self) -> None:
         self.data_processor = DataProcessor()
-        self.data_processor._load(category="All_beauty", frac=0.01)
+        self.data_processor._load(category=self.category, frac=self.frac)
         self.data_processor._process_reviews(clean_text=False)
-        self.data_processor._embedd_reviews_and_split(embedding="tf-idf")
+        self.data_processor._embedd_reviews_and_split(embedding=self.embedding)
+
+
+class XGBoostSentimentClassifier(SentimentClassifier):
+
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+        self.model_name = "xgb-sentiment-classifier"
 
     def _train(self) -> None:
         self.xgb = XGBClassifier(objective='multi:softmax', num_class=3, eval_metric='mlogloss', use_label_encoder=False)
@@ -44,5 +53,33 @@ class XGBoostSentimentClassifier:
         )
 
 
-class RandomForestSentimentClassifier:
-    pass
+class RandomForestSentimentClassifier(SentimentClassifier):
+
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+        self.model_name = "randomforest-sentiment-classifier"
+
+    def _train(self) -> None:
+        self.rf = RandomForestClassifier(n_estimators=100, random_state=42)
+        self.rf.fit(self.data_processor.X_train, self.data_processor.y_train)
+        self.y_pred = self.rf.predict(self.data_processor.X_test)
+        self.trained = True
+
+    def _analyse(self) -> None:
+        if not self.trained:
+            logger.warning("Please train your model first.")
+            return
+        report = classification_report(
+            y_true=self.data_processor.y_test,
+            y_pred=self.y_pred,
+            target_names=self.data_processor.label_encoder.classes_,
+        )
+        logger.info(report)
+
+    def _make_figures(self) -> None:
+        make_confusion_matrix(
+            y_test=self.data_processor.y_test,
+            y_pred=self.y_pred,
+            classes=self.data_processor.label_encoder.classes_,
+            file_path=FIGURES_PATH / self.model_name / "confusion_matrix.png"
+        )
