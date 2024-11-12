e1ef43b824eb7afdc86b4e6b8f859ae6da3a582f
LucaNyckees <nyckees.luca@gmail.com>
2024-11-12 13:43:17 +0100

    added: loading products

--- src/database/populate/load/products.py
+++ src/database/populate/load/products.py
@@ -1,15 +1,56 @@
+from rich.console import Console
+import pandas as pd
+import json
+import os
+from psycopg.sql import SQL, Identifier, Placeholder
 
-cols_mapping = {
+from src.paths import DATA_PATH, RESOURCES_PATH
+from src.database.connection import connect
+
+
+with open(RESOURCES_PATH / "amazon_product_categories.json") as f:
+    categories_dict = json.load(f)
+
+
+db_cols_to_inserted_cols_mapping = {
     "parent_asin": "parent_asin",
     "name": "title",
-    "main_category": "",
-    "average_rating": "",
-    "rating_number": "",
+    "main_category": "main_category",
+    "average_rating": "average_rating",
+    "rating_number": "rating_number",
     "features": "",
-    "description": "",
-    "price": "",
-    "image_urls": "",
-    "store": "",
-    "categories": "",
-    "details": ""
+    "description": "description",
+    "price": "price",
+    "image_urls": "images",
+    "store": "store",
+    "categories": "categories",
+    "details": "details"
 }
+
+
+def load_products():
+
+    products_dataframe = None
+
+    for category in categories_dict["categories"]:
+
+        file_path = DATA_PATH / f"{category}.jsonl"
+        if not os.path.isfile(file_path):
+            continue
+
+        if products_dataframe is None:
+            products_dataframe = pd.read_json(file_path, lines=True)
+        else:
+            to_append = pd.read_json(file_path, lines=True)
+            products_dataframe = products_dataframe.append(to_append, ignore_index=True)
+
+        products_list_of_dicts = products_dataframe.to_dict("records")
+
+    with connect(db_key="main") as conn:
+        with conn.cursor() as cur:
+            insert_query = SQL("INSERT INTO rs_amazon_products ({db_cols}) VALUES ({inserted_cols})").format(
+                db_cols=", ".join(map(Identifier, db_cols_to_inserted_cols_mapping.keys())),
+                inserted_cols=", ".join(map(Placeholder, db_cols_to_inserted_cols_mapping.values())),
+                )
+            cur.executemany(query=insert_query, params_seq=products_list_of_dicts)
+    Console().log("Countries loaded")
