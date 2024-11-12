f86b25c42de365bc1fd98248b68e8789daa9116a
LucaNyckees <nyckees.luca@gmail.com>
2024-11-12 11:03:14 +0100

    sqlalchemy conn

--- src/database/config.py
+++ src/database/config.py
@@ -1,14 +1,16 @@
 from urllib.parse import quote
 from toml import load
+from sqlalchemy import URL
 
 from src.paths import ROOT
+
+
+config = load(ROOT / "config.toml")
+
 
 def format_db_url_postgresql(user: str, password: str, host: str, port: int, name: str) -> str:
     password = quote(password, safe="$^)]")
     return f"postgresql://{user}:{password}@{host}:{port}/{name}"
-
-
-config = load(ROOT / "config.toml")
 
 
 def get_db_url_from_key(db_key: str) -> str:
@@ -16,3 +18,21 @@ def get_db_url_from_key(db_key: str) -> str:
     db_config = config["dbs"][db_key]
 
     return format_db_url_postgresql(**db_config)
+
+
+def format_db_url_sqlalchemy(**kwargs) -> URL:
+    d = kwargs.copy()
+
+    keys = ("user", "password", "host", "port", "name")
+    cred = {}
+    for k in keys:
+        assert k in d, f"format_db_url_sqlalchemy must be provided with proper credentials : {keys}"
+        cred[k] = d[k]
+    return URL.create(
+        "postgresql",
+        username=cred["user"],
+        password=cred["password"],
+        host=cred["host"],
+        database=cred["name"],
+        port=cred["port"],
+    )
