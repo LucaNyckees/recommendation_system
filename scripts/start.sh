480336c8c064d1f9fa09b53f692b46c1220c102d
LucaNyckees <nyckees.luca@gmail.com>
2024-11-19 16:52:47 +0100

    echos

--- scripts/start.sh
+++ scripts/start.sh
@@ -7,7 +7,16 @@ echo "Database is ready!"
 
 # Run dataset download and load commands
 alembic upgrade head
+echo "alembic upgrade successful."
+
 make download-data
+echo "datasets downloaded."
+
 make load-data
+echo "data loaded to db."
+
 uvicorn src.fastapi_app.main:app  --host 0.0.0.0 --port 8000
+echo "fastapi app launched."
+
 python3.10 . src/dashboard/dashboard.py
+echo "dash app launched."
