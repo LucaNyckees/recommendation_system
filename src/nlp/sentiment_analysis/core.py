0f31f907fca3d0df89afb3f102299919463661d0
LucaNyckees <nyckees.luca@gmail.com>
2024-11-22 13:26:55 +0100

    minor log toplevel

--- src/nlp/sentiment_analysis/core.py
+++ src/nlp/sentiment_analysis/core.py
@@ -1,4 +1,5 @@
 from src.nlp.sentiment_analysis.classifiers import XGBoostSentimentClassifier, RandomForestSentimentClassifier
+from src.log.logger import logger
 
 
 def sentiment_classifier_pipeline(category: str, embedding: str, frac: float) -> None:
@@ -6,6 +7,7 @@ def sentiment_classifier_pipeline(category: str, embedding: str, frac: float) ->
     Trains and stores two models per category.
     TODO : actually store models with MLFlow
     """
+    logger.info(f"Running sentiment classifiers pipeline | category={category}, embedding={embedding}, frac={frac}")
     assert embedding in {"tf-idf", "bert"}, "embedding not valid"
 
     xgb_sentiment_classifier = XGBoostSentimentClassifier(
