727947ef50c7d9085fe0230287835c4bb43d7d55
LucaNyckees <nyckees.luca@gmail.com>
2024-11-22 13:20:00 +0100

    minor

--- src/nlp/sentiment_analysis/__init__.py
+++ src/nlp/sentiment_analysis/__init__.py
@@ -9,7 +9,6 @@ def run_sentiment_classifier_pipeline(
     category: Annotated[str, typer.Option(help="Amazon data category")] = "All_beauty",
     embedding: Annotated[str, typer.Option(help="Word embedding")] = "tf-idf",
     frac: Annotated[float, typer.Option(help="Amazon data category")] = 0.01,
-    # debug: Annotated[bool, typer.Option(help="Amazon data category")] = False,
 ) -> None:
     
     from src.nlp.sentiment_analysis.core import sentiment_classifier_pipeline
