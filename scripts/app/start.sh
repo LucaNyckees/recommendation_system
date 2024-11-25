9d3fb92802685d921a2066873441c9e54018a704
LucaNyckees <nyckees.luca@gmail.com>
2024-11-25 17:48:38 +0100

    added: mlflow to docker

--- scripts/app/start.sh
+++ scripts/app/start.sh
@@ -5,6 +5,9 @@ echo "datasets downloaded."
 make load-data
 echo "data loaded to db."
 
+mlflow server --hot 0.0.0.0 --port 5000
+echo "mlflow server launched."
+
 uvicorn src.fastapi_app.main:app --host 0.0.0.0 --port 8000 &
 echo "fastapi app launched."
 
