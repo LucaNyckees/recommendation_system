63ccd150241c794c011c04e470dcb56e76bd81b6
LucaNyckees <nyckees.luca@gmail.com>
2024-11-26 13:46:54 +0100

    fixed: minor

--- src/database/populate/load/users.py
+++ src/database/populate/load/users.py
@@ -24,32 +24,13 @@ db_cols_to_inserted_cols_mapping = {
 }
 
 cities = {
-    "paris": 0.075,       # Approx. population: 11,000,000
-    "berlin": 0.062,      # Approx. population: 9,000,000
-    "madrid": 0.058,      # Approx. population: 8,500,000
-    "rome": 0.051,        # Approx. population: 7,500,000
-    "london": 0.125,      # Approx. population: 18,500,000
-    "amsterdam": 0.012,   # Approx. population: 1,800,000
-    "brussels": 0.015,    # Approx. population: 2,200,000
-    "vienna": 0.017,      # Approx. population: 2,500,000
-    "warsaw": 0.021,      # Approx. population: 3,100,000
-    "stockholm": 0.012,   # Approx. population: 1,800,000
-    "oslo": 0.008,        # Approx. population: 1,200,000
-    "helsinki": 0.007,    # Approx. population: 1,050,000
-    "copenhagen": 0.011,  # Approx. population: 1,650,000
-    "lisbon": 0.013,      # Approx. population: 1,900,000
-    "athens": 0.021,      # Approx. population: 3,100,000
-    "prague": 0.014,      # Approx. population: 2,000,000
-    "budapest": 0.015,    # Approx. population: 2,200,000
-    "dublin": 0.010,      # Approx. population: 1,500,000
-    "zagreb": 0.007,      # Approx. population: 1,050,000
-    "sofia": 0.008,       # Approx. population: 1,200,000
-    "belgrade": 0.009,    # Approx. population: 1,350,000
-    "bucharest": 0.020    # Approx. population: 3,000,000
+    "paris": 1/3,
+    "berlin": 1/3,
+    "madrid": 1/3,
 }
 
 # Verify the sum of probabilities
-assert abs(sum(cities.values()) - 1) < 1e-9, "Values do not sum to 1"
+assert abs(sum(cities.values()) - 1) < 1e-9, f"Values do not sum to 1, but to {sum(cities.values())}"
 
 genders = {"female": 0.45, "male": 0.45, "non-binary": 0.05, "other": 0.05}
 
@@ -71,8 +52,8 @@ def load_users() -> None:
 
             logger.info(f"Generating random data for {nb_users} users")
             users_dataframe["age"] = np.random.normal(loc=35, scale=7, size=nb_users)
-            users_dataframe["gender"] = np.random.choice(genders.keys(), size=nb_users, p=genders.values())
-            users_dataframe["city"] = np.random.choice(cities.keys(), size=nb_users, p=cities.values())
+            users_dataframe["gender"] = np.random.choice(list(genders.keys()), size=nb_users, p=list(genders.values()))
+            users_dataframe["city"] = np.random.choice(list(cities.keys()), size=nb_users, p=list(cities.values()))
 
             logger.info("Inserting values...")
             insert_values(
