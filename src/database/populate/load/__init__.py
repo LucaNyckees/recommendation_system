de1f09d3a87e5deeab16f92d22d8c7b58951c8f6
LucaNyckees <nyckees.luca@gmail.com>
2024-11-26 13:44:14 +0100

    added: loading users

--- src/database/populate/load/__init__.py
+++ src/database/populate/load/__init__.py
@@ -13,3 +13,12 @@ def load_datasets() -> None:
     load_reviews()
 
     return None
+
+
+@app.command(name="generated-users")
+def load_datasets() -> None:
+    from .users import load_users
+
+    load_users()
+
+    return None
