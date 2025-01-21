8512dfe6f6177f36a2f562bd79f792f82c9b803b
LucaNyckees <nyckees.luca@gmail.com>
2025-01-21 12:15:12 +0100

    revision of db: adding vector cols

---
+++ src/database/alembic/versions/3d5d5f752450_embedding_text_data.py
@@ -0,0 +1,30 @@
+"""embedding text data
+
+Revision ID: 3d5d5f752450
+Revises: e45a460919ef
+Create Date: 2025-01-19 16:27:41.851361
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = '3d5d5f752450'
+down_revision: Union[str, None] = 'e45a460919ef'
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    op.add_column("rs_amazon_reviews", sa.Column("title_and_text", sa.ARRAY(), autoincrement=False, nullable=True))
+    op.add_column("rs_amazon_products", sa.Column("name", sa.ARRAY(), autoincrement=False, nullable=True))
+    op.add_column("rs_amazon_products", sa.Column("description", sa.ARRAY(), autoincrement=False, nullable=True))
+
+
+def downgrade() -> None:
+    op.drop_column("rs_amazon_reviews", "title_and_text")
+    op.drop_column("rs_amazon_products", "name")
+    op.drop_column("rs_amazon_products", "description")
